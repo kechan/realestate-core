100f9b1dcce449bd60a00c199a1fc1537e2b46ed
CHAN,KELVIN <kechan@gmail.com>
2022-10-29 00:08:49 -0400

    added more env related and bucket ext.

--- realestate_core/common/class_extensions.py
+++ realestate_core/common/class_extensions.py
@@ -7,3 +7,26 @@ from pathlib import Path
 Path.ls = lambda x: list(x.iterdir())
 Path.lf = lambda pth, pat='*': list(pth.glob(pat))
 Path.rlf = lambda pth, pat='*': list(pth.rglob(pat))
+
+try:
+  from google.cloud.storage.bucket import Bucket
+  def _download_from_gcs(self, src, dst_dir=None, debug=False):
+    try:
+      blob = self.blob(src)
+      if dst_dir is None:
+        # download to local and use the same filename
+        target_path = str(Path(src).name)
+      else:
+        target_path = str(Path(dst_dir)/Path(src).name)
+
+      blob.download_to_filename(target_path)
+    except Exception as ex:
+      if debug:
+        raise   
+      else:
+        print('Something has gone wrong. Please debug.')
+
+  Bucket.download = _download_from_gcs
+except:
+  print('google.cloud.storage.bucket.Bucket not found, please pip install google-cloud-storage')
+
--- realestate_core/common/run_config.py
+++ realestate_core/common/run_config.py
@@ -3,6 +3,11 @@ from __future__ import print_function
 from pathlib import Path
 
 bOnColab = Path('/content').exists()
+bOnKaggle = Path('/kaggle/').exists()
+bOnGCPVM = Path('/home/jupyter').exists()
+bOnPaperspace = Path('/notebooks').exists()
+bOnLocal = Path('/Users/kelvinchan').exists()
+
 
 if bOnColab:
   home = Path('/content/drive/MyDrive')
