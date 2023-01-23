e0ba4b6b838cd325c83b80db6227ba38068db65f
CHAN,KELVIN <kechan@gmail.com>
2023-01-23 17:55:42 -0500

    Path.lfre for ls with regex on filename

--- realestate_core/common/class_extensions.py
+++ realestate_core/common/class_extensions.py
@@ -8,6 +8,9 @@ Path.ls = lambda x: list(x.iterdir())
 Path.lf = lambda pth, pat='*': list(pth.glob(pat))
 Path.rlf = lambda pth, pat='*': list(pth.rglob(pat))
 
+import re
+Path.lfre = lambda pth, regex='.*': [p for p in pth.glob('*') if re.search(regex, p.name)]
+
 try:
   from google.cloud.storage.bucket import Bucket
 
